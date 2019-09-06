ae4a7c774db4b7257fb0478b8103a5e650f7727e
sole100 <mgm11299@hotmail.com>
2019-09-07 00:55:28 +0200

    probamos con el protocolo mysql

--- mysqli.inc.php
+++ mysqli.inc.php
@@ -1,5 +1,5 @@
 <?php
-$cfg_servidor="10.130.6.222";
+$cfg_servidor="mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
 $cfg_usuario="user";
 $cfg_password="pass";
 $cfg_basephp1="prueba";
