40c8d3baa0db74bc9927d4299620369003b00c16
sole100 <mgm11299@hotmail.com>
2019-09-29 17:50:02 +0200

    protocolo mysql

--- mysqli.inc.php
+++ mysqli.inc.php
@@ -1,5 +1,5 @@
 <?php
-$cfg_servidor="mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/";
+$cfg_servidor="mysql://mysql:3306";
 $cfg_usuario="user";
 $cfg_password="pass";
 $cfg_basephp1="prueba";
